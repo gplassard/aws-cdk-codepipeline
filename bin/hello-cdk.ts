f59a3ce6da5be75518cbf9213f5c0a1e15b1d376
Gabriel Plassard <gabriel.plassard@gmail.com>
2018-12-28 01:32:26 +0100

    codepipeline

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -3,5 +3,5 @@ import cdk = require('@aws-cdk/cdk');
 import { HelloCdkStack } from '../lib/hello-cdk-stack';
 
 const app = new cdk.App();
-new HelloCdkStack(app, 'HelloCdkStack');
+new HelloCdkStack(app, 'HelloCdkStack', {env: {region: 'eu-west-1'}});
 app.run();
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,9 +1,36 @@
-import cdk = require('@aws-cdk/cdk');
+import cdk = require("@aws-cdk/cdk");
+import codebuild = require("@aws-cdk/aws-codebuild");
+import codepipeline = require("@aws-cdk/aws-codepipeline");
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
     super(parent, name, props);
 
     // The code that defines your stack goes here
+
+    const project = new codebuild.PipelineProject(this, "MyProject", {
+      projectName: "MyProject"
+    });
+
+    const pipeline = new codepipeline.Pipeline(this, "MyPipeline", {
+      pipelineName: "MyPipeline"
+    });
+
+    const source = pipeline.addStage("Source");
+    new codepipeline.GitHubSourceAction(this, "GithubSource", {
+      stage: source,
+      owner: "gplassard",
+      repo: "aws-cdk-codepipeline",
+      branch: "master",
+      oauthToken: new cdk.SecretParameter(this, "GithubOAuthSecret", {
+        ssmParameter: "github-gplassard-oauth"
+      }).value
+    });
+
+    const build = pipeline.addStage("Build");
+    new codebuild.PipelineBuildAction(this, "PipelineBuild", {
+      stage: build,
+      project: project
+    });
   }
 }
